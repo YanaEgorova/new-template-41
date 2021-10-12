c9956024c531a054e79de5e1a7d96e070b43eefd
Yana <yanaegorova69.3@gmail.com>
2021-10-12 17:14:57 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,138 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Nail Clippers',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Magnifying glass zoom in for precise nail trimming.',
+            },
+            {
+                text: 'Mini emery board that\'s curvy and easy to use on tiny child nails.',
+            },
+            {
+                text: 'Built in LED light illuminates in low lighting.',
+            },
+            {
+                text: 'Easy grip design.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.png', './img/img1-5.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Art Storage Box',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'Material: Wood.',
+            },
+            {
+                text: 'Mounting Type: Tabletop.',
+            },
+            {
+                text: 'Item Dimensions: LxWxH 17 x 11.5 x 6 inches.',
+            },
+            {
+                text: 'Number of Compartments: 25.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Airbed',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Approximate inflation time 4 3/4 minutes.',
+            },
+            {
+                text: 'The sleeping surface is covered in soft flocking for extra comfort, and the indented sides keep your fitted sheets from slipping.',
+            },
+            {
+                text: 'The convenient hand carry bag is perfect for storage and transport.',
+            },
+            {
+                text: 'Weight capacity: 600 lbs.',
+            },
+            {
+                text: 'Dimensions: 60" x 80" x 22".',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Sewing Machine',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: '32 Stitch Applications to cover your basic sewing needs.',
+            },
+            {
+                text: 'Repair seams and create durable hems.',
+            },
+            {
+                text: 'Lightweight and Portable - great for small spaces & easy to store!',
+            },
+            {
+                text: 'Top drop-in bobbin to easily insert & monitor bobbin thread supply.',
+            },
+            {
+                text: 'Free arm to sew hems and cuffs with ease.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Pressure Cooker',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: '22 quart polished aluminum stovetop canner and pressure cooker with a deluxe pressure gauge dial.',
+            },
+            {
+                text: 'Selective pressure control with 3 cooking pressure options: 5, 10 or 15 PSI; overpressure steam release safety components.',
+            },
+            {
+                text: 'Heavy gauge, rustproof aluminum construction; sure locking lid system; pressure monitoring device built into handle.',
+            },
+            {
+                text: 'Includes 2 canning racks to hold 16 pint size jars or 26 half pint size jars when double stacked or 7 quart size jars.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png', './img/img5-4.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Towel Warmer',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                title: 'Oversized Capacity',
+                text: 'Extra-large towel warmer easily accommodates up to two 40’’ x 70’’ bath towels, robes, PJ’s or your favorite throw blanket.',
+            },
+            {
+                title: 'Peace Of Mind',
+                text: 'Never worry about turning off your towel warmer with its energy-saving timer for safety.',
+            },
+            {
+                title: 'Even Heating',
+                text: 'The entire towel is heated inside and out so you don’t have to worry about cold spots.',
+            },
+            {
+                title: 'Perfect Timing',
+                text: 'Have warmth when you need it by choosing between four (4) timer settings of 15, 30, 45 and 60 minutes.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png'],
+    },
+]
